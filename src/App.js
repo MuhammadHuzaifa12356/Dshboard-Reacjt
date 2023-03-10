c7a192a6f21744d2dc09738b260c02b143f293ec
Muhammad Huzaifa <huzaifajt2525@gmail.com>
2023-03-10 18:02:58 +0500

    work complete

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import AppRouter from "./config/approuter";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Dashboard } from "@mui/icons-material";
+
+function App() {
+  return (
+    <>
+      <AppRouter />
+      
+    </>
+  );
+}
+
+export default App;
